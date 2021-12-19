043bfd65f5bc597b28864999eea20bdc332389dc
dani-buntu <sio2guanoeleo@gmail.com>
2021-12-19 17:37:01 +0100

    ok modifies 1- 2- 3- filenames

--- print_table.php
+++ print_table.php
@@ -2,10 +2,28 @@
 
 header('Content-Type:text/plain');
 
+//fieisright function that checks if the name of the file is correct
+//a correct file name is a file that starts with a number followed by a "-"
+//return 0 if the file name is correct
+//return -1 if the file name starts with a number, followed by a "-" and contains the world "skip"
+//return 1 if the file name is incorrec
+function fileisright($folder_and_file)
+{
+	if (preg_match("/^[0-9]+-/", $folder_and_file))
+	{
+		if (preg_match("/skip/", $folder_and_file))
+			return -1;
+		else
+			return 0;
+	}
+	else
+		return 1;
+}
+
 function fixpath($str)
 {
-  $str2 = "/data/vhosts/coralesantalessandro.com/httpdocs/wordpress/";
-  return(str_replace($str2,"", $str));
+$str2 = "/data/vhosts/coralesantalessandro.com/httpdocs/wordpress/";
+return(str_replace($str2,"", $str));
 }
 
 #function to print in a table the output of the function read_files
@@ -15,120 +33,131 @@ function fixpath($str)
 #print on the columns the name of the files (third array depth first index)
 #every file is clickable and will open the file in a new window
 function print_table($array){
-  echo '<table>'; ?>
-  <style>
-  table {
-    border: 1px solid #ccc;
-    border-collapse: collapse;
-    margin: 0;
-    padding: 0;
-    width: 100%;
-    table-layout: fixed;
-  }
+echo '<table>'; ?>
+<style>
+table {
+	border: 1px solid #ccc;
+	border-collapse: collapse;
+	margin: 0;
+	padding: 0;
+	width: 100%;
+	table-layout: fixed;
+}
 
-  table caption {
-    font-size: 1.5em;
-    margin: .5em 0 .75em;
-  }
+table caption {
+	font-size: 1.5em;
+	margin: .5em 0 .75em;
+}
 
-  table tr {
-    background-color: black;
-    border: 1px solid #ddd;
-    padding: .35em;
-  }
+table tr {
+	background-color: black;
+	border: 1px solid #ddd;
+	padding: .35em;
+}
 
-  table th,
-  table td {
-    padding: .625em;
-    text-align: center;
-  }
+table th,
+table td {
+	padding: .625em;
+	text-align: center;
+}
 
-  table th {
-    font-size: .85em;
-    letter-spacing: .1em;
-    text-transform: uppercase;
-  }
+table th {
+	font-size: .85em;
+	letter-spacing: .1em;
+	text-transform: uppercase;
+}
 
-  @media screen and (max-width: 600px) {
-    table {
-      border: 0;
-    }
+@media screen and (max-width: 600px) {
+	table {
+	border: 0;
+	}
 
-    table caption {
-      font-size: 1.3em;
-    }
-    
-    table thead {
-      border: none;
-      clip: rect(0 0 0 0);
-      height: 1px;
-      margin: -1px;
-      overflow: hidden;
-      padding: 0;
-      position: absolute;
-      width: 1px;
-    }
-    
-    table tr {
-      border-bottom: 3px solid #ddd;
-      display: block;
-      margin-bottom: .625em;
-    }
-    
-    table td {
-      border-bottom: 1px solid #ddd;
-      display: block;
-      font-size: .8em;
-      text-align: right;
-    }
-    
-    table td::before {
-      /*
-      * aria-label has no advantage, it won't be read inside a table
-      content: attr(aria-label);
-      */
-      content: attr(data-label);
-      float: left;
-      font-weight: bold;
-      text-transform: uppercase;
-    }
-    
-    table td:last-child {
-      border-bottom: 0;
-    }
-  }
-  </style>
-  <thead>
-    <tr>
-      <th scope="col">Nome</th>
-      <th scope="col">Tipologia 1</th>
-      <th scope="col">Tipologia 2</th>
-      <th scope="col">Spartito</th>
-      <th scope="col">Ascolta</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-    <?php
-  foreach($array as $key => $value){
-    echo '<tr>';
-    echo '<td>'.$key.'</td>';
-    foreach($value as $key2 => $folder_and_file){
-      //if in the filename is present the world "skip", it will leave the column empty
-      //else it will echo '<td><a href="http://www.coralesantalessandro.com/wordpress/'.fixpath($folder_and_file[0])."/".$folder_and_file[1].'" target="_blank">'.$folder_and_file[1].'</a></td>';
-      if(strpos($folder_and_file[1], "skip") !== false){
-        echo '<td></td>';
-      }
-      else{
-        echo '<td><a href="http://www.coralesantalessandro.com/wordpress/'.fixpath($folder_and_file[0])."/".$folder_and_file[1].'" target="_blank">'.$folder_and_file[1].'</a></td>';
-      }
-    }
-    echo '</tr>';
-  }
-  echo '</table>'; ?>
-    </tr>
-  </tbody>
-  <?php
+	table caption {
+	font-size: 1.3em;
+	}
+	
+	table thead {
+	border: none;
+	clip: rect(0 0 0 0);
+	height: 1px;
+	margin: -1px;
+	overflow: hidden;
+	padding: 0;
+	position: absolute;
+	width: 1px;
+	}
+	
+	table tr {
+	border-bottom: 3px solid #ddd;
+	display: block;
+	margin-bottom: .625em;
+	}
+	
+	table td {
+	border-bottom: 1px solid #ddd;
+	display: block;
+	font-size: .8em;
+	text-align: right;
+	}
+	
+	table td::before {
+	/*
+	* aria-label has no advantage, it won't be read inside a table
+	content: attr(aria-label);
+	*/
+	content: attr(data-label);
+	float: left;
+	font-weight: bold;
+	text-transform: uppercase;
+	}
+	
+	table td:last-child {
+	border-bottom: 0;
+	}
+}
+</style>
+<thead>
+	<tr>
+	<th scope="col">Titolo</th>
+	<th scope="col">Soprani</th>
+	<th scope="col">Contralti</th>
+	<th scope="col">Tenori</th>
+	<th scope="col">Bassi</th>
+	<th scope="col">Spartito</th>
+	<th scope="col">Ascolta</th>
+	</tr>
+</thead>
+<tbody>
+	<tr>
+	<?php
+foreach($array as $key => $value){
+	echo '<tr>';
+	echo '<td>'.$key.'</td>';
+	foreach($value as $key2 => $folder_and_file){
+	if(fileisright($folder_and_file[1]) == 0)
+		//echo '<td><a href="http://www.coralesantalessandro.com/wordpress/'.fixpath($folder_and_file[0])."/".$folder_and_file[1].'" target="_blank">'.$folder_and_file[1].'</a></td>';
+		//echo the same thing as before but the name of the file should not contain the first 2 characters
+		//this is because the first 2 characters are used to identify the order of the files
+		//the first 2 characters are the number of the file and the "-"
+		echo '<td><a href="http://www.coralesantalessandro.com/wordpress/'.fixpath($folder_and_file[0])."/".substr($folder_and_file[1],2).".mp3".'" target="_blank">'.substr($folder_and_file[1],2).".mp3".'</a></td>';
+	elseif(fileisright($folder_and_file[1]) == -1)
+		echo '<td></td>';
+	
+		//if in the filename is present the world "skip", it will leave the column empty
+	//else it will echo '<td><a href="http://www.coralesantalessandro.com/wordpress/'.fixpath($folder_and_file[0])."/".$folder_and_file[1].'" target="_blank">'.$folder_and_file[1].'</a></td>';
+	/*	if(strpos($folder_and_file[1], "skip") !== false){
+			echo '<td></td>';
+		}
+		else{
+			echo '<td><a href="http://www.coralesantalessandro.com/wordpress/'.fixpath($folder_and_file[0])."/".$folder_and_file[1].'" target="_blank">'.$folder_and_file[1].'</a></td>';
+		}*/
+	}
+	echo '</tr>';
+}
+echo '</table>'; ?>
+	</tr>
+</tbody>
+<?php
 }
 
 #php function that reads the files from the directory passed to it
@@ -141,26 +170,26 @@ function read_files($dir) {
 	$dir_array = scandir($dir);
 	foreach ($dir_array as $key => $value) {
 		if ($value != "." && $value != "..") 
-    {
+	{
 			if (is_dir($dir . "/" . $value)) 
-      {
+	{
 				$files[$value] = read_files($dir . "/" . $value);
 			} 
-      else 
-      {
-        $ext = pathinfo($value, PATHINFO_EXTENSION);
+	else 
+	{
+		$ext = pathinfo($value, PATHINFO_EXTENSION);
 				if ($ext == "mp3" || $ext == "pdf" || $ext == "txt") 
-        {
-          #check if $value has number into it
-          if(preg_match('/\-\d/', $value))
-          {
-            $sametipe[] = array($value);
-          }
-          else
-          {
-            $files[] = array($dir, $value, $sametipe);
-				  }
-        }
+		{
+		#check if $value has number into it
+		if(preg_match('/\-\d/', $value))
+		{
+			$sametipe[] = array($value);
+		}
+		else
+		{
+			$files[] = array($dir, $value, $sametipe);
+				}
+		}
 			}
 		}
 	}
